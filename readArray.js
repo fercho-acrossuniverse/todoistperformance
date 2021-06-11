a3813b09cb40f71e663a566b542843f09c63846b
fercho-acrossuniverse <automation.2021.rolling@gmail.com>
2021-06-10 21:51:43 -0500

    Adding artillery script to test todoist api

---
+++ readArray.js
@@ -0,0 +1,14 @@
+module.exports = {
+    setJSONBody: setJSONBody,
+    logHeaders: logHeaders
+  }
+  
+  function setJSONBody(requestParams, context, ee, next) {
+    console.log(response.context);
+    return next(); // MUST be called for the scenario to continue
+  }
+  
+  function logHeaders(requestParams, response, context, ee, next) {
+    //console.log(response.headers);
+    return next(); // MUST be called for the scenario to continue
+  }
